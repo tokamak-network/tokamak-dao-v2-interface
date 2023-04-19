0d7a669f73d05d628ffe7603742e399292792a53
cd4761 <cd4761@gmail.com>
2023-04-19 11:40:04 +0900

    add agenda side card section

--- src/pages/agenda.tsx
+++ src/pages/agenda.tsx
@@ -1,12 +1,27 @@
 import { Flex } from "@chakra-ui/react"
 import { useAgenda } from "@/hooks/agenda/useAgendas"
+import { AgendaList } from './components/agenda/AgendaList';
+import { AgendaSide } from "./components/agenda/AgendaSide";
 
 function Agenda () {
   const agendas = useAgenda()
   console.log(agendas)
   return (
-    <Flex>
-      Agenda
+    <Flex
+      minW={'1200px'}
+      w={'100%'}
+      minH={'89vh'}
+      flexDir={'row'}
+      justifyContent={'center'}
+      alignItems={'start'}
+      my={'35px'}
+    >
+      <AgendaList 
+        agendaList={agendas}
+      />
+      <AgendaSide
+        agendaList={agendas}
+      />
     </Flex>
   )
 }
